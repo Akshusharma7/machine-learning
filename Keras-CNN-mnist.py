ec31d3e449f9898a727f891c935dbe15f168c496
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-01-10 08:31:30 +0530

    Update Keras-CNN-mnist.py

--- Keras-CNN-mnist.py
+++ Keras-CNN-mnist.py
@@ -4,3 +4,7 @@ from keras.layers import Dense, Conv2D, Flatten
 import matplotlib.pyplot as plt
 from keras.datasets import mnist
 from keras.utils import to_categorical
+
+
+#download mnist data and split into train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
