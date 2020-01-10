b91ca639d52f2c7579928f4af94e102aba208c93
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-01-10 08:32:14 +0530

    Update Keras-CNN-mnist.py

--- Keras-CNN-mnist.py
+++ Keras-CNN-mnist.py
@@ -8,3 +8,10 @@ from keras.utils import to_categorical
 
 #download mnist data and split into train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+#check image shape
+print(X_train[0].shape) #(28,28)
+
+#reshape data to fit model
+X_train = X_train.reshape(60000,28,28,1)
+X_test = X_test.reshape(10000,28,28,1)
