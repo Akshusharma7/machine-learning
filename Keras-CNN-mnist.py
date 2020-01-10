eec4fde9b14b74915a3801f1452ab889e45644cd
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-01-10 08:31:09 +0530

    Create Keras-CNN-mnist.py

---
+++ Keras-CNN-mnist.py
@@ -0,0 +1,6 @@
+#Import Libraries
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+import matplotlib.pyplot as plt
+from keras.datasets import mnist
+from keras.utils import to_categorical
