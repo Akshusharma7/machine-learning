49be8d315a5eed1163354c01271597d5e255d046
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-06-15 20:36:33 +0530

    Update Keras-CNN-mnist.py

--- Keras-CNN-mnist.py
+++ Keras-CNN-mnist.py
@@ -10,7 +10,6 @@ from keras.utils import to_categorical
 #When using Jupyter Notebook else comment it
 %matplotlib inline
 
-
 #download mnist data and split into train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
